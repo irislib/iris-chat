12ca6f936ff8743dada575fc8615a70b37879825
Dev <dev@local>
2026-04-26 00:38:58 +0300

    Add nos.lol to default relays

--- src/lib/defaultRelays.ts
+++ src/lib/defaultRelays.ts
@@ -1,6 +1,7 @@
 export const DEFAULT_RELAYS = [
   'wss://relay.primal.net',
   'wss://relay.damus.io',
+  'wss://nos.lol',
   'wss://relay.snort.social',
   'wss://temp.iris.to',
   'wss://offchain.pub',
